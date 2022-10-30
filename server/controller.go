87191e5e797f904209f8ee067dfcb1d83c2ea5f1
Flatpacks <97040578+Flatpacks@users.noreply.github.com>
2022-10-30 22:16:43 +0000

    bumps up version

--- server/controller.go
+++ server/controller.go
@@ -52,3 +52,57 @@ func (ctl *AlertController) AddAlert(c echo.Context) error {
 		return c.JSON(http.StatusOK, NewParseRequestErrorResponse())
 	}
 	return c.JSON(http.StatusOK, NewSuccessResponse(alert))
+}
+
+func (ctl *AlertController) RemoveAlert(c echo.Context) error {
+	data := GetJSON(c)
+	if data == nil || data["id"] == nil {
+		return c.JSON(http.StatusOK, NewInvalidDataErrorResponse())
+	}
+
+	alert, err := ctl.alertService.GetAlertByID(int64(data["id"].(float64)))
+	if err != nil {
+		return c.JSON(http.StatusOK, NewDataNotFoundErrorResponse())
+	}
+	_, err = ctl.alertService.RemoveAlert(alert)
+	if err != nil {
+		log.Warn(err.Error())
+		return c.JSON(http.StatusOK, NewInvalidDataErrorResponse())
+	}
+	return c.JSON(http.StatusOK, NewSuccessResponse(nil))
+}
+
+func (ctl *AlertController) UpdateAlert(c echo.Context) error {
+	data := GetJSON(c)
+	if data == nil || data["id"] == nil || data["price"] == nil {
+		return c.JSON(http.StatusOK, NewInvalidDataErrorResponse())
+	}
+
+	alert, err := ctl.alertService.GetAlertByID(int64(data["id"].(float64)))
+	if err != nil {
+		return c.JSON(http.StatusOK, NewDataNotFoundErrorResponse())
+	}
+	alert.Price = data["price"].(float64)
+	_, err = ctl.alertService.UpdateAlert(alert)
+	if err != nil {
+		return c.JSON(http.StatusOK, NewParseRequestErrorResponse())
+	}
+	return c.JSON(http.StatusOK, NewSuccessResponse(alert))
+}
+
+func (ctl *AlertController) GetAlerts(c echo.Context) error {
+	alerts, err := ctl.alertService.GetAlerts()
+	if err != nil {
+		return c.JSON(http.StatusOK, NewInternalErrorResponse())
+	}
+	return c.JSON(http.StatusOK, NewSuccessResponse(alerts))
+}
+
+func GetJSON(c echo.Context) map[string]interface{} {
+	var json map[string]interface{} = map[string]interface{}{}
+
+	if err := c.Bind(&json); err != nil {
+		return nil
+	}
+	return json
+}
