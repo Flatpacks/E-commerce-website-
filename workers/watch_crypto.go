8f8596d86509458474a7f60e3f95e4b1081b2f47
Flatpacks <97040578+Flatpacks@users.noreply.github.com>
2023-02-12 06:24:38 +0000

    typo in documentation

--- workers/watch_crypto.go
+++ workers/watch_crypto.go
@@ -14,4 +14,9 @@ var QUEUE_KEY_WATCH_CRYPTO = "tasks.send_email"
 var QUEUE_ADDR_WATCH_CRYPTO = "localhost:6379"
 
 // redis
-var rdb 
+var rdb *redis.Client
+
+func WatchCryptoWorker() {
+	ctx := context.TODO()
+
+	// we only c
